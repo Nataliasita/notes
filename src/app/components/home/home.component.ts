250eed6676f89f35357f4a87177917bb296b3d47
Natalia Gonzalez <73859972+Nataliasita@users.noreply.github.com>
2022-01-22 21:22:58 -0500

    validacion de form

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    title ='example react form';
+
+    public formLogin!:FormGroup 
+
+  constructor( private formBuilder:FormBuilder) {
+
+   }
 
   ngOnInit(): void {
+    this.formLogin = this.formBuilder.group({
+      email:['',
+      [
+        Validators.required,
+        Validators.email
+      ],
+    ],
+      password:['',
+      [
+        Validators.required,
+        Validators.minLength(6)
+      ]
+       
+
+      ]
+    });
+
   }
+
+  send(): any{
+    console.log(this.formLogin!.value)
+  }
+
 
 }
