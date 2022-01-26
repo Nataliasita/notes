631d2a09a8c3b9ab5df17a6d6e2ec800310f0cb7
Natalia Gonzalez <73859972+Nataliasita@users.noreply.github.com>
2022-01-25 19:36:14 -0500

    ajustes interfaz 2

--- src/app/components/board/dashboard/dashboard.component.ts
+++ src/app/components/board/dashboard/dashboard.component.ts
@@ -2,18 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import{ DataNotes } from 'src/app/models/data-notes'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  messageEdit!:string;
 
   public formNote!:FormGroup 
 
   notesArray: DataNotes[] = [
     {id:1,title:'Note 1', description:'prueba'},
-    {id:2,title:'Note 2', description:'prueba'}
+    {id:2,title:'Note 2', description:'prueba'},
+    {id:3,title:'Note 3', description:'prueba'}
   ];
 
   selectedNote: DataNotes =new DataNotes();
@@ -42,15 +45,20 @@ export class DashboardComponent implements OnInit {
     if(this.selectedNote.id === 0){
     this.selectedNote.id = this.notesArray.length + 1;
     this.notesArray.push(this.selectedNote);
+    
   }
     this.selectedNote = new DataNotes();
+    this.messageEdit='';
   }
 
   editNote(note:DataNotes){
     this.selectedNote = note;
+    this.messageEdit='Edit Note';
   }
-  // deleteNote(){
-  //   this.notesArray = this.notesArray.filter(x => x != this.selectedNote);
-  // }
+  deleteNote(){
+    this.notesArray = this.notesArray.filter(x => x != this.selectedNote);
+    this.selectedNote = new DataNotes();
+    this.messageEdit='';
+  }
 
 }
